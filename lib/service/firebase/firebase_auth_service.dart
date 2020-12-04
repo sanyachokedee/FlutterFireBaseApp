64418d5e2f551425a5945eb63bf8c6fe2ed35144
MooMOO <ing118199@gmail.com>
2020-12-04 12:54:51 +0700

    FirebaseAPP Day10th

--- lib/service/firebase/firebase_auth_service.dart
+++ lib/service/firebase/firebase_auth_service.dart
@@ -7,9 +7,7 @@ import 'package:flutter_facebook_login/flutter_facebook_login.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 import 'package:http/http.dart' as http;
 
-
 class FirebaseAuthService {
-
   static FirebaseAuth _firebaseAuth = FirebaseAuth.instance;
   static GoogleSignIn _googleSignIn = GoogleSignIn();
   static FacebookLogin _facebookSignIn = FacebookLogin();
@@ -20,31 +18,34 @@ class FirebaseAuthService {
   // ==============================================================================
   Future createUserWithPhone(String phone, BuildContext context) async {
     _firebaseAuth.verifyPhoneNumber(
-      phoneNumber: phone, 
-      timeout: Duration(seconds: 0),
-      verificationCompleted: (AuthCredential authCredential){
-          _firebaseAuth.signInWithCredential(authCredential).then((UserCredential result){
+        phoneNumber: phone,
+        timeout: Duration(seconds: 0),
+        verificationCompleted: (AuthCredential authCredential) {
+          _firebaseAuth
+              .signInWithCredential(authCredential)
+              .then((UserCredential result) {
             Navigator.of(context).pop(); // to pop the dialog box
             Navigator.of(context).pushReplacementNamed('/home');
           }).catchError((e) {
             print("Error complete");
             return "error";
           });
-      }, 
-      verificationFailed: (FirebaseAuthException exception) {
+        },
+        verificationFailed: (FirebaseAuthException exception) {
           return "error";
-      },
-      codeSent: (String verificationId, [int forceResendingToken]) {
-        final _codeController = TextEditingController();
-        showDialog(
-          context: context,
-          barrierDismissible: false,
-          builder: (context) => AlertDialog(
+        },
+        codeSent: (String verificationId, [int forceResendingToken]) {
+          final _codeController = TextEditingController();
+          showDialog(
+            context: context,
+            barrierDismissible: false,
+            builder: (context) => AlertDialog(
               title: Text("ป้อนรหัส OTP ที่ได้รับ"),
               content: Column(
                 mainAxisSize: MainAxisSize.min,
                 children: <Widget>[
                   TextField(
+                    //controller มาเพื่อทำการรับค่าจาก ที่ป้อน otp
                     controller: _codeController,
                     keyboardType: TextInputType.number,
                   )
@@ -56,26 +57,29 @@ class FirebaseAuthService {
                   textColor: Colors.white,
                   color: Colors.green,
                   onPressed: () {
-                      var _credential = PhoneAuthProvider.credential(verificationId: verificationId,
-                          smsCode: _codeController.text.trim());
-                      _firebaseAuth.signInWithCredential(_credential).then((UserCredential result){
-                        Navigator.of(context).pop(); // to pop the dialog box
-                        Navigator.of(context).pushReplacementNamed('/home');
-                      }).catchError((e) {
-                        // return "error";
-                        BottomSheetWidget().bottomSheet(context, "มีข้อผิดพลาด", "ป้อนรหัส OTP ไม่ถูกต้อง");
-                      });
+                    var _credential = PhoneAuthProvider.credential(
+                        verificationId: verificationId,
+                        smsCode: _codeController.text.trim());
+                    _firebaseAuth
+                        .signInWithCredential(_credential)
+                        .then((UserCredential result) {
+                      Navigator.of(context).pop(); // to pop the dialog box
+                      Navigator.of(context).pushReplacementNamed('/home');
+                    }).catchError((e) {
+                      // return "error";
+                      BottomSheetWidget().bottomSheet(
+                          context, "มีข้อผิดพลาด", "ป้อนรหัส OTP ไม่ถูกต้อง");
+                    });
                   },
                 )
               ],
             ),
-        );
-      },
-      codeAutoRetrievalTimeout: (String verificationId) {
+          );
+        },
+        codeAutoRetrievalTimeout: (String verificationId) {
           verificationId = verificationId;
-      });
+        });
   }
-
 
   // ==============================================================================
   // สร้างฟังก์ชันสำหรับเข้าระบบด้วย Google Gmail
@@ -88,14 +92,16 @@ class FirebaseAuthService {
       accessToken: _googleAuth.accessToken,
     );
 
-    _firebaseAuth.signInWithCredential(_credential).then((UserCredential result){
+    _firebaseAuth
+        .signInWithCredential(_credential)
+        .then((UserCredential result) {
       Navigator.of(context).pushReplacementNamed('/home');
     }).catchError((e) {
       // return "error";
-      BottomSheetWidget().bottomSheet(context, "มีข้อผิดพลาด", "ข้อมูลการ Sign In ไม่ถูกต้อง");
-    }); 
+      BottomSheetWidget()
+          .bottomSheet(context, "มีข้อผิดพลาด", "ข้อมูลการ Sign In ไม่ถูกต้อง");
+    });
   }
-
 
   // ==============================================================================
   // สร้างฟังก์ชันสำหรับเข้าระบบด้วย Facebook
@@ -107,38 +113,45 @@ class FirebaseAuthService {
 
         // final FacebookAccessToken accessToken = result.accessToken;
         // BottomSheetWidget().bottomSheet(context,"เข้าสู่ระบบสำเร็จ","Token: ${accessToken.token}\nUser id: ${accessToken.userId}\nExpires: ${accessToken.expires}\nPermissions: ${accessToken.permissions}\nDeclined permissions: ${accessToken.declinedPermissions}");
-        
+
         final String token = result.accessToken.token;
-        final response = await http.get('https://graph.facebook.com/v2.12/me?fields=name,first_name,last_name,email&access_token=${token}');
+        final response = await http.get(
+            'https://graph.facebook.com/v2.12/me?fields=name,first_name,last_name,email&access_token=${token}');
         final profile = jsonDecode(response.body);
         print(profile);
 
         Navigator.pushReplacementNamed(context, '/home');
         break;
       case FacebookLoginStatus.cancelledByUser:
-        BottomSheetWidget().bottomSheet(context,"มีข้อผิดพลาด","ผู้ใช้ยกเลิกการเข้าใช้งาน");
+        BottomSheetWidget()
+            .bottomSheet(context, "มีข้อผิดพลาด", "ผู้ใช้ยกเลิกการเข้าใช้งาน");
         break;
       case FacebookLoginStatus.error:
-        BottomSheetWidget().bottomSheet(context,"มีข้อผิดพลาด","มีข้อผิดพลาด ${result.errorMessage} ในระหว่างการเข้าใช้งาน");
+        BottomSheetWidget().bottomSheet(context, "มีข้อผิดพลาด",
+            "มีข้อผิดพลาด ${result.errorMessage} ในระหว่างการเข้าใช้งาน");
         break;
     }
   }
 
-
   // ==============================================================================
   // สร้างฟังก์ชันสำหรับการ login ด้วย Email
   // ==============================================================================
-  Future firebaseSignIn(BuildContext context, String email, String password) async {
+  Future firebaseSignIn(
+      BuildContext context, String email, String password) async {
     try {
-      await _firebaseAuth.signInWithEmailAndPassword(email: email, password: password);
+      await _firebaseAuth.signInWithEmailAndPassword(
+          email: email, password: password);
       Navigator.pushReplacementNamed(context, '/home');
     } on FirebaseAuthException catch (e) {
       if (e.code == 'invalid-email') {
-        BottomSheetWidget().bottomSheet(context, "มีข้อผิดพลาด", "ป้อนอีเมล์ไม่ถูกต้อง");
+        BottomSheetWidget()
+            .bottomSheet(context, "มีข้อผิดพลาด", "ป้อนอีเมล์ไม่ถูกต้อง");
       } else if (e.code == 'user-not-found') {
-        BottomSheetWidget().bottomSheet(context, "มีข้อผิดพลาด", "ไม่พบอีเมล์นี้ในระบบ");
+        BottomSheetWidget()
+            .bottomSheet(context, "มีข้อผิดพลาด", "ไม่พบอีเมล์นี้ในระบบ");
       } else if (e.code == 'wrong-password') {
-        BottomSheetWidget().bottomSheet(context, "มีข้อผิดพลาด", "รหัสผ่านไม่ถูกต้อง");
+        BottomSheetWidget()
+            .bottomSheet(context, "มีข้อผิดพลาด", "รหัสผ่านไม่ถูกต้อง");
       } else {
         print(e.code);
       }
@@ -150,17 +163,22 @@ class FirebaseAuthService {
   // ==============================================================================
   // สร้างฟังก์ชันสำหรับการ regsiter ด้วย Email
   // ==============================================================================
-  Future firebaseRegister(BuildContext context,String email, String password) async {
+  Future firebaseRegister(
+      BuildContext context, String email, String password) async {
     try {
-      await _firebaseAuth.createUserWithEmailAndPassword(email: email, password: password);
-      Navigator.pushReplacementNamed(context, '/home');      
+      await _firebaseAuth.createUserWithEmailAndPassword(
+          email: email, password: password);
+      Navigator.pushReplacementNamed(context, '/home');
     } on FirebaseAuthException catch (e) {
       if (e.code == 'weak-password') {
-        BottomSheetWidget().bottomSheet(context, "มีข้อผิดพลาด", "รหัสผ่านสั้นหรือง่ายเกินไป ไม่ปลอดภัย");
+        BottomSheetWidget().bottomSheet(
+            context, "มีข้อผิดพลาด", "รหัสผ่านสั้นหรือง่ายเกินไป ไม่ปลอดภัย");
       } else if (e.code == 'email-already-in-use') {
-        BottomSheetWidget().bottomSheet(context, "มีข้อผิดพลาด", "มีบัญชีนี้อยู่แล้วในระบบ ลองใช้อีเมล์อื่น");
-      } else if(e.code == 'invalid-email'){
-        BottomSheetWidget().bottomSheet(context, "มีข้อผิดพลาด", "รูปแบบอีเมล์ไม่ถูกต้อง");
+        BottomSheetWidget().bottomSheet(context, "มีข้อผิดพลาด",
+            "มีบัญชีนี้อยู่แล้วในระบบ ลองใช้อีเมล์อื่น");
+      } else if (e.code == 'invalid-email') {
+        BottomSheetWidget()
+            .bottomSheet(context, "มีข้อผิดพลาด", "รูปแบบอีเมล์ไม่ถูกต้อง");
       }
       return false;
     } catch (e) {
@@ -187,5 +205,4 @@ class FirebaseAuthService {
       print(e);
     }
   }
-
 }
